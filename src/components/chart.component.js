94b7bcbef41c09530f5d41d135b5da967f2078d0
Hasan-aga <careerforma@gmail.com>
2022-12-06 00:19:53 +0300

    create a area chart

--- src/components/chart.component.js
+++ src/components/chart.component.js
@@ -5,7 +5,27 @@ import {LineChart} from 'react-native-gifted-charts';
 export default function Chart({styles, data}) {
   return (
     <View style={styles.chartContainer}>
-      <LineChart data={data} adjustToWidth={true} />
+      <LineChart
+        data={data}
+        adjustToWidth={true}
+        areaChart
+        hideDataPoints
+        isAnimated
+        animationDuration={1200}
+        startFillColor="#0BA5A4"
+        startOpacity={1}
+        endOpacity={0.3}
+        initialSpacing={0}
+        spacing={30}
+        thickness={5}
+        hideRules
+        hideYAxisText
+        yAxisColor="#0BA5A4"
+        showVerticalLines
+        verticalLinesColor="rgba(14,164,164,0.5)"
+        xAxisColor="#0BA5A4"
+        color="#0BA5A4"
+      />
     </View>
   );
 }
