775475c3701b633ab6c15bdce1e3f432ed1ae424
Hasan-aga <careerforma@gmail.com>
2022-12-12 21:42:43 +0300

    solve chart distance problem

--- src/utils/adjustElevationData.ts
+++ src/utils/adjustElevationData.ts
@@ -8,22 +8,28 @@ export type Elevation = {
 export const relateDistanceAcrossLegs = (legs: Leg[]): Elevation => {
   const elevation_range: Elevation = [];
 
+  let legIncrement = 0;
   legs.forEach((leg, index) => {
     //  each elevation_range is array of [distance, elevation]
     // distance of current leg must be incremented by last distance of prev leg
     // if index === 0 -> increment = 0
     //  if index > 0 -> increment = prevLeg.elevation_range.at(-1)[0]
+
     const lastElevationRange =
       index > 0
         ? legs[index - 1].elevation_range[
             legs[index - 1].elevation_range.length - 1
           ]
         : undefined;
-    const increment = lastElevationRange ? lastElevationRange[0] : 0;
+    const increment = lastElevationRange
+      ? lastElevationRange[0] + legIncrement
+      : 0;
     console.log(increment);
+    legIncrement += increment;
 
     const fixedElevationRange = leg.elevation_range.map(elevation => {
       const distance = elevation[0] + increment;
+
       return {x: distance, y: elevation[1]};
     });
 
