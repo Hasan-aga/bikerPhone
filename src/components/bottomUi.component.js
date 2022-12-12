d53661e692fe5d5dbc953e47bd887e0fd63d6124
Hasan-aga <careerforma@gmail.com>
2022-12-12 16:59:31 +0300

    remove card animation

--- src/components/bottomUi.component.js
+++ src/components/bottomUi.component.js
@@ -6,23 +6,6 @@ import {
 } from 'react-native';
 import React, {useEffect, useRef, useState} from 'react';
 export default function BottomUi({styles, cardVisible, children}) {
-  const yPosition = useRef(new Animated.Value(0)).current;
-
-  useEffect(() => {
-    Animated.timing(yPosition, {
-      toValue: cardVisible ? 0 : 200,
-      duration: 250,
-      useNativeDriver: true,
-    }).start();
-  }, [cardVisible]);
-
-  const AnimatedView = Animated.createAnimatedComponent(
-    TouchableWithoutFeedback,
-  );
-
-  return (
-    <AnimatedView style={{transform: [{translateY: yPosition}]}}>
-      <View style={styles.bottomUi}>{children}</View>
-    </AnimatedView>
-  );
+  const updatedStyle = {...styles.bottomUi, bottom: cardVisible ? 0 : -200};
+  return <View style={updatedStyle}>{children}</View>;
 }
